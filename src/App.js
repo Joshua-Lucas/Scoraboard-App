83ab2b0da38128bf51ee06049acee27d73a46ffe
Joshua Lucas <joshuamlucas91@gmail.com>
2020-03-19 17:27:24 -0400

    create test folder and add some styling to app.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div class="bg-blue-500 h-32 flex justify-center">
-        <h1 class="text-red-500 text-4xl m-auto">Hello World</h1>
+      <div class="bg-blue-500 my-auto w-full flex flex-col items-center">
+        <h1 class="text-red-500 text-4xl ">Hello World</h1>
+        <h1>Test</h1>
       </div>
+
     );
   }
 }
