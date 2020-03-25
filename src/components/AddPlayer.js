8d592d79877ceb31e538481a5ea6389197669386
Joshua Lucas <joshuamlucas91@gmail.com>
2020-03-25 17:20:34 -0400

    refactored addPlayer to use refs

--- src/components/AddPlayer.js
+++ src/components/AddPlayer.js
@@ -1,40 +1,43 @@
-import React, {Component} from 'react';
+import React from 'react';
+import { Consumer } from './context';
 
-class AddPlayer extends Component {
+const AddPlayer = () =>{
 
-    state = {
-      value: ''
-    };
+   const playerInput = React.createRef();
   
-    handleValueChange = (e) => {
-      this.setState({ value: e.target.value });
-    }
+
   
-    handleSubmit = (e) => {
-      e.preventDefault();
-      this.props.addPlayer(this.state.value);
-      this.setState({ value: '' });
-    }
-  
-    render() {
-      return (
-        <form className=" rounded-b bg-blue-500 flex justify-around w-full "onSubmit={this.handleSubmit}>
-          <input 
-            className="bg-blue-800 rounded w-4/5 m-4 p-2" 
-            type="text"
-            value={this.state.value}
-            onChange={this.handleValueChange}
-            placeholder="Enter a player's name"
-          />
-          
-          <input 
-            className ="w-1/5 rounded m-4 py-2"
-            type="submit"
-            value="Add Player"
-          />
-        </form>
+    return(
+        <Consumer>
+        
+            {({actions}) => {
+                
+                const handleSubmit = (e) => {
+                    e.preventDefault();
+                    actions.addPlayer(playerInput.current.value);
+                    e.currentTarget.reset();
+                  }
+                
+                return (
+                    <form className=" rounded-b bg-blue-500 flex justify-around w-full "onSubmit={handleSubmit}>
+                    <input 
+                        className="bg-blue-700 rounded w-4/5 m-4 p-2" 
+                        type="text"
+                        ref={playerInput}
+                        placeholder="Enter a player's name"
+                    />
+                    
+                    <input 
+                        className ="w-1/5 rounded m-4 py-2"
+                        type="submit"
+                        value="Add Player"
+                    />
+                    </form>
+                );  
+            }}
+        </Consumer>
       );
-    }
+
   }
 
 
