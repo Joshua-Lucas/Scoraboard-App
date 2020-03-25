81f589c707da556855bf8e915597620004e2a697
Joshua Lucas <joshuamlucas91@gmail.com>
2020-03-25 18:59:40 -0400

    from adds to the remove player handle

--- src/components/Player.js
+++ src/components/Player.js
@@ -9,14 +9,16 @@ class Player extends PureComponent
         const {index} = this.props;
     return(
         <Consumer>
-            {({players}) => 
+            {({players, actions}) => 
                 (
                     <div className=" bg-gray-200 flex w-full m-1 p-2 py-5 w-1/2 ">
-                        <div className=" flex-1">
-                            <button className="text-gray-400 hover:text-red-900 pr-2 ">&#10008;</button>
+                        <div className=" flex-1 text-2xl">
+                            <button className="text-gray-400  hover:text-red-900 pr-2 " onClick={() => actions.removePlayer(players[index].id)}>&#10008;</button>
                             {players[index].name}
                         </div>
-                        <Score />
+                        <Score
+                            index={index}
+                        />
                     </div>
                    
                 )
