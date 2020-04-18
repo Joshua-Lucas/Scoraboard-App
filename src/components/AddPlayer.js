b68ab239985a9099172a732a32e15d136f2e6bad
Joshua Lucas <joshuamlucas91@gmail.com>
2020-04-18 18:28:31 -0400

    fix padding on iput element in AddPlayer componenet

--- src/components/AddPlayer.js
+++ src/components/AddPlayer.js
@@ -19,14 +19,14 @@ const AddPlayer = () =>{
                 return (
                     <form className=" mt-1 rounded-b bg-blue-500 flex flex-col items-center md:flex-row md:justify-around w-full "onSubmit={handleSubmit}>
                         <input 
-                            className="w-4/5 m-2 py-2 bg-blue-700 rounded focus:bg-gray-300 md:m-4 md:p-2" 
+                            className="w-4/5 m-2 p-2 bg-blue-700 rounded focus:bg-gray-300 md:m-4 " 
                             type="text"
                             ref={playerInput}
                             placeholder="Enter a player's name"
                         />
                         
                         <input 
-                            className =" mb-2 py-2 w-4/5 rounded hover:bg-gray-300 hover:font-medium cursor-pointer md:w-1/5 md:m-4 md:py-2"
+                            className =" mb-2 py-2 w-4/5 rounded hover:bg-gray-300 hover:font-medium cursor-pointer md:w-1/5 md:m-4"
                             type="submit"
                             value="Add Player"
                         />
